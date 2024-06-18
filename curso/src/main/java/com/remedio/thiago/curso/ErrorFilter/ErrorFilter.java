9d5bbb1709265055dc9c096c5d3c20772937962a
Thiago Martins <158003020+ThiagoMartins2001@users.noreply.github.com>
2024-06-18 09:50:18 -0300

    error information filtering 2

--- curso/src/main/java/com/remedio/thiago/curso/ErrorFilter/ErrorFilter.java
+++ curso/src/main/java/com/remedio/thiago/curso/ErrorFilter/ErrorFilter.java
@@ -1,6 +1,8 @@
 package com.remedio.thiago.curso.ErrorFilter;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,19 @@ public class ErrorFilter {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> ErrorFilter404(){
         return ResponseEntity.notFound().build();
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> ErrorFilter400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErros::new).toList());
+    }
+
+    public record DadosErros(String campo, String messagem){
+        public DadosErros(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
 
     }
 }
